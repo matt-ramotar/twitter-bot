6030336ca70124e3569add2d8d4816d4a28b3fab
mramotar <mramotar@dropbox.com>
2022-05-14 20:26:07 -0400

    Fix write

--- log-action/log.js
+++ log-action/log.js
@@ -4,7 +4,13 @@ import oldest from '../retweets.js'
 function log(){
     const newest = process.env.DID_RETWEET
     const latest = [...newest, ...oldest]
-    fs.writeFile('./retweets.js', latest, err => {
+    const content = `
+    const retweets = ${latest};
+
+    export default retweets;
+    `
+    
+    fs.writeFile('./retweets.js', content, err => {
         console.log(err)
     })
 
