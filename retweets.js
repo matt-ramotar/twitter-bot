0113d942e7453aa5917bfe05efa5266774e023a1
matt-ramotar <matt-ramotar@users.noreply.github.com>
2022-05-15 01:07:14 +0000

    Auto log

--- retweets.js
+++ retweets.js
@@ -1,5 +1,5 @@
-const retweets = [
-    "1525537985441239042"
-]
 
-export default retweets
+    const retweets = ["1525538068081557505","1525537985441239042"];
+
+    export default retweets;
+    
